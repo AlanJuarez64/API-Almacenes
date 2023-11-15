b3b03161f39f94b0394b0ecbc5a2ae64b4093433
Alan Juárez <alanjuarez.n64@gmail.com>
2023-11-15 00:19:14 -0300

    Se actualizó el testing en base a las autenticaciones

--- tests/Feature/ArticuloTest.php
+++ tests/Feature/ArticuloTest.php
@@ -5,34 +5,52 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Http;
 use App\Models\Articulo;
+use App\Models\User;
 
 class ArticuloTest extends TestCase
 {
     public function testVerTodosLosArticulos(){
-        $response = $this->get('/api/articulos/');
+        $token = $this->simularAutenticacion();
+        
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/articulos/');
 
         $response->assertStatus(200)
             ->assertJsonStructure(['*' => [
-                'ID_Articulo', 'ID_Usuario', 'ID_Producto', 'Estado',
+                'ID_Articulo', 'id', 'ID_Producto', 'Estado',
                 'created_at', 'updated_at'
                 ]]);
     }
 
     public function testBuscarArticuloExistente()
-    {
-        $response = $this->get('/api/articulos/1');
+    {        
+        $token = $this->simularAutenticacion();
+        
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/articulos/1');
 
         $response->assertStatus(200)
             ->assertJsonStructure([
-                'ID_Articulo', 'ID_Usuario', 'ID_Producto', 'Estado',
+                'ID_Articulo', 'id', 'ID_Producto', 'Estado',
                 'created_at', 'updated_at'
                 ]);
     }
 
     public function testBuscarArticuloNoExistente()
-    {
-        $response = $this->get('/api/articulos/999');
+    {        
+        $token = $this->simularAutenticacion();
+        
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/articulos/999');
+
         $response->assertStatus(404)
             ->assertJsonStructure(['error']);
 
@@ -40,8 +58,14 @@ class ArticuloTest extends TestCase
 
     public function testCambiarEstadoDeArticuloExistente()
     {
-        $data = ['estado' => 'En camino'];
-        $response = $this->put('/api/articulos/1', $data);
+        $data = ['Estado' => 'En camino'];
+
+        $token = $this->simularAutenticacion();
+        
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->put('/api/articulos/1', $data);
 
         $response->assertStatus(200)
             ->assertJson(["message" => "Estado del artículo modificado correctamente"]);
@@ -53,8 +77,14 @@ class ArticuloTest extends TestCase
 
     public function testCambiarEstadoDeArticuloNoExistente()
     {
-        $data = ['estado' => 'En camino'];
-        $response = $this->put('/api/articulos/999', $data);
+        $data = ['Estado' => 'En el almacen'];
+        
+        $token = $this->simularAutenticacion();
+        
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->put('/api/articulos/999', $data);
 
         $response->assertStatus(404)
             ->assertJson(["error" => "Artículo no encontrado"]);
@@ -63,13 +93,17 @@ class ArticuloTest extends TestCase
     public function testRegistrarArticuloValido()
     {
         $datos = [
-            'ID_Usuario' => 1,
+            'id' => 4,
             'ID_Producto' => 2,
-            'Estado' => 'En espera'
+            'Estado' => 'En el almacen'
         ];
 
- 
-        $response = $this->post('/api/articulos', $datos);
+        $token = $this->simularAutenticacion();
+        
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/articulos', $datos);
 
         $response->assertStatus(200);
 
@@ -79,17 +113,36 @@ class ArticuloTest extends TestCase
     public function testRegistrarArticuloInvalido()
     {
         $datos = [
-            'ID_Usuario' => 999,
+            'id' => 999,
             'ID_Producto' => 999,
             'Estado' => 'a'
         ];
 
- 
-        $response = $this->post('/api/articulos', $datos);
-
-        $response->assertStatus(400);
-                
+        $token = $this->simularAutenticacion();
         
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/articulos', $datos);
+
+        $response->assertStatus(400); 
+    }
+
+    private function simularAutenticacion()
+    {
+        $user = User::factory()->create();
+        
+        $response = Http::post('http://localhost:8001/oauth/token', [
+            'grant_type' => 'password',
+            'client_id' => env('CLIENT_ID'),
+            'client_secret' => env('CLIENT_SECRET'),
+            'username' => "$user->email",
+            'password' => 'password',
+        ]);
+
+        $token = $response->json('access_token');
+
+        return $token;
     }
 
 }
